e7a7094263b6ab11ad4c8b8881a1d58856e2dd5c
yuzo-hikida <0601uzo@gmail.com>
2019-09-16 14:51:46 +0900

    ToDoを追加ボタンの名前を新規作成に名前変更

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,7 +3,7 @@
 
 <h1 class="page-header">ToDo一覧</h1>
 <p class="text-right">
-  <a class="btn btn-success" href="/todo/create">ToDoを追加</a>
+  <a class="btn btn-success" href="/todo/create">新規作成</a>
 </p>
 <table class="table">
   <thead class="thead-light">
